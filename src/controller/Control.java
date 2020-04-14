3653b3b8488ca16c27851da01c86483a1b6e2679
johns <johns@DESKTOP-JNQV98N>
2020-04-14 19:26:56 -0300

    Adição do logotipo na tela do Onibus

--- src/controller/Control.java
+++ src/controller/Control.java
@@ -27,6 +27,7 @@ public class Control {
     }
 
     public static ArrayList<Object> getAllClientes() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        System.out.println("controller.Control.getAllClientes()");
         return (ArrayList<Object>) ClienteDAO.getInstance().retrieveAll();
     }
 
